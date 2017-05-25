26ff6705ead315d77a25dfa2260402eaa3b21a51
suzuha <kosuneco58@yahoo.co.jp>
2017-05-25 22:54:03 +0000

    tasklist update,changed message

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,10 @@ class TasksController < ApplicationController
     @task=Task.new(task_params)
     
     if @task.save
-      flash[:success]='タスクが正常に投稿されました'
+      flash[:success]='Task が正常に投稿されました'
       redirect_to @task
     else
-      flash.now[:danger]='タスクが投稿されませんでした'
+      flash.now[:danger]='Task が投稿されませんでした'
       render :new
     end
   end
@@ -28,6 +28,15 @@ class TasksController < ApplicationController
   end
   
   def update
+    @task=Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
   
   def destroy
